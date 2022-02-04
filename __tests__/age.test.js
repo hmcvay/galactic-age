fc2f44e438cbc3faa0e3aaca3e4276ba5587cc68
hmcvay <hayleymcvay8@gmail.com>
2022-02-04 09:58:15 -0800

    Add third test to calculate age on Venus; also updated argument for new Age to be 1, instead of 30, so the math is simpler in the tests.

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -5,14 +5,18 @@ describe ('Age', () => {
   let age;
 
   beforeEach(() => {
-    age = new Age(30);
+    age = new Age(1);
   });
 
   test('should construct Age object', () => {
-    expect(age.earthAge).toEqual(30)
+    expect(age.earthAge).toEqual(1)
   });
 
   test('should calculate Mercury age based on Earth age', () => {
-    expect(age.calcMercuryAge()).toEqual(125)
+    expect(age.calcMercuryAge()).toEqual(4.166666666666667)
   });
+
+  test('should calculate Venus age based on Earth age', () => {
+    expect(age.calcVenusAge()).toEqual(1.612903225806452)
+  })
 });
