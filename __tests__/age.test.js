4111caddc47d6f01904179e85308161837cb1224
hmcvay <hayleymcvay8@gmail.com>
2022-02-04 13:45:37 -0800

    Refactor age.test.js and age.js to better structor Age class to allow for comparing life on other planets to life expectancy.

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,35 +1,39 @@
 import Age from "../src/js/age.js"
 
+let age;
+
+beforeEach(() => {
+  age = new Age(5);
+  age.calcAges();
+});
+
 describe ('Age', () => {
-
-  let age;
-
-  beforeEach(() => {
-    age = new Age(5);
-  });
 
   test('should construct Age object', () => {
     expect(age.earthAge).toEqual(5)
   });
+});
+
+describe ('calcAges', () => {
 
   test('should calculate Mercury age based on Earth age', () => {
-    expect(age.calcMercuryAge()).toEqual(21)
+    expect(age.mercuryAge).toEqual(21)
   });
 
   test('should calculate Venus age based on Earth age', () => {
-    expect(age.calcVenusAge()).toEqual(8)
+    expect(age.venusAge).toEqual(8)
   });
 
   test('should calculate Mars age based on Earth age', () => {
-    expect(age.calcMarsAge()).toEqual(9)
+    expect(age.marsAge).toEqual(9)
   });
 
   test('should calculate Jupiter age based on Earth age', () => {
-    expect(age.calcJupiterAge()).toEqual(59)
+    expect(age.jupiterAge).toEqual(59)
   });
 
-  test('should return true if user age on planet is greater than life expectancy age', () => {
-    let lifeExpectancy = 78;
-    expect(age.calcLifeExpect()).toEqual("true")
-  });
 });
+
+// describe ('calcLifeExpect', () => {
+
+// })
--- src/js/age.js
+++ src/js/age.js
@@ -1,21 +1,20 @@
 export default class Age {
   constructor(earthAge) {
-    this.earthAge = earthAge
+    this.earthAge = earthAge;
+    this.mercuryAge = 0;
+    this.venusAge = 0;
+    this.marsAge = 0;
+    this.jupiterAge = 0;
   }
 
-  calcMercuryAge() {
-    return Math.round(this.earthAge/.24)
-  }
-
-  calcVenusAge() {
-    return Math.round(this.earthAge/.62)
-  }
-
-  calcMarsAge() {
-    return Math.round(this.earthAge * 1.88)
-  }
-
-  calcJupiterAge() {
-    return Math.round(this.earthAge * 11.86)
+  calcAges() {
+    this.mercuryAge = Math.round(this.earthAge/.24);
+    this.venusAge = Math.round(this.earthAge/.62);
+    this.marsAge = Math.round(this.earthAge * 1.88);
+    this.jupiterAge = Math.round(this.earthAge * 11.86);
   }
 }
+//   calcLifeExpect() {
+
+//   }
+// }
