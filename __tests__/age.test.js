b68659300f0c1b0622a84bdd1838732de20c43f3
hmcvay <hayleymcvay8@gmail.com>
2022-02-04 14:49:14 -0800

    UPDATE 30 to be the age passed in to tests, instead of 5, so we can have wider range of results to work with in testing. All tests still pass.

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -3,7 +3,7 @@ import Age from "../src/js/age.js"
 let age;
 
 beforeEach(() => {
-  age = new Age(5);
+  age = new Age(30);
   age.calcAges();
   age.calcLifeExpect();
 });
@@ -11,26 +11,26 @@ beforeEach(() => {
 describe ('Age', () => {
 
   test('should construct Age object', () => {
-    expect(age.earthAge).toEqual(5)
+    expect(age.earthAge).toEqual(30)
   });
 });
 
 describe ('calcAges', () => {
 
   test('should calculate Mercury age based on Earth age', () => {
-    expect(age.mercuryAge).toEqual(21)
+    expect(age.mercuryAge).toEqual(125)
   });
 
   test('should calculate Venus age based on Earth age', () => {
-    expect(age.venusAge).toEqual(8)
+    expect(age.venusAge).toEqual(48)
   });
 
   test('should calculate Mars age based on Earth age', () => {
-    expect(age.marsAge).toEqual(9)
+    expect(age.marsAge).toEqual(56)
   });
 
   test('should calculate Jupiter age based on Earth age', () => {
-    expect(age.jupiterAge).toEqual(59)
+    expect(age.jupiterAge).toEqual(356)
   });
 
 });
@@ -42,19 +42,23 @@ describe ('calcLifeExpect', () => {
   });
 
   test('should calculate years left of life based on age on Mercury', () => {
-    expect(age.mercuryLifeExpect).toEqual(51)
+    expect(age.mercuryLifeExpect).toEqual(-53)
   });
 
   test('should calculate years left of life based on age on Venus', () => {
-    expect(age.venusLifeExpect).toEqual(64)
+    expect(age.venusLifeExpect).toEqual(24)
   });
 
   test('should calculate years left of life based on age on Mars', () => {
-    expect(age.marsLifeExpect).toEqual(63)
+    expect(age.marsLifeExpect).toEqual(16)
   });
 
   test('should calculate years left of life based on age on Jupiter', () => {
-    expect(age.jupiterLifeExpect).toEqual(13)
+    expect(age.jupiterLifeExpect).toEqual(-284)
   });
   
 });
+
+describe ('checkNegative', () => {
+
+})
